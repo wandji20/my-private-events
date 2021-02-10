a98130215f45b87613ca09f2b54be3faa91281fd
wandji20 <wbertrand10@gmail.com>
2021-02-10 10:58:14 +0100

    Add new session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    user = User.find_by_id_userid(params[:userid])
+    if user
+      sessions[:userid] = user.id
+      redirect_to root_path, notice: 'Welcome #{user.name}\nYou are signed in '
+    else 
+      render 'new', alert:'Invalid User Id'
+    end
+  end
+
+
 end
