9b0b94ed4da111f5fc12622fd0562113b9a70d62
wandji20 <wbertrand10@gmail.com>
2021-02-13 23:44:53 +0100

    Update attendance create action to check attended events before adding a new event

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,23 @@
 class AttendancesController < ApplicationController
+  def new 
+
+  end
+
+  def create
+
+    event = current_user.attended_events.find_by_id(params[:attended_event_id])
+    if !event
+      @attendance = current_user.attendances.build(:attended_event_id => params[:attended_event_id])
+      if @attendance.save
+        redirect_to root_path
+      else
+        redirect_to new_event_path
+      end
+    else
+      redirect_to root_path
+    end
+
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :events
+  resources :attendances
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
