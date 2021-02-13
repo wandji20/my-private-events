2047aa8828cad097c61c4d4e7432d737a916b4b4
wandji20 <wbertrand10@gmail.com>
2021-02-13 13:05:27 +0100

    Add class method to Event and use it to display both past and upcoming events in the events index view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def Event.past
+    self.select do |event|
+      event.date < Date.today.to_s
+    end
+  end
+
+  def Event.upcoming
+    self.select do |event|
+      event.date > Date.today.to_s
+    end
+  end
 end
