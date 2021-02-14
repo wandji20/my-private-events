c2c33b3fd55fda1ea2556c7489855ac9418c7bbd
wandji20 <wbertrand10@gmail.com>
2021-02-14 20:23:27 +0100

    Add dependent on event creator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :created_events, class_name: 'Event', foreign_key: "creator_id"
+  has_many :created_events, class_name: 'Event', foreign_key: "creator_id", dependent: :destroy
   has_many :attendances, foreign_key: 'attendee_id'
   has_many :attended_events, class_name: 'Event', through: 'attendances'
 end
