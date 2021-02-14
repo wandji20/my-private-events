ba6af686532ebd5b351c1462bb1437bf8c91c21a
wandji20 <wbertrand10@gmail.com>
2021-02-14 22:40:36 +0100

    Add indices to attendance relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_191421) do
+ActiveRecord::Schema.define(version: 2021_02_14_213649) do
 
   create_table "attendances", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "attendee_id"
     t.integer "attended_event_id"
+    t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
   end
 
   create_table "events", force: :cascade do |t|
